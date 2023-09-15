14eeb76d12e2416c39460cc657e061b179c9d576
ckwong <chunkang.wong94@gmail.com>
2023-09-16 00:30:05 +0800

    Updated dependencies

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -4,13 +4,19 @@ module.exports = {
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
+
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
+  },
+
+  docs: {
+    autodocs: true
   }
 }
