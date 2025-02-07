0042897357dee5366c605f038ef41ffab52d7cd6
chunkangwong <chunkang.wong94@gmail.com>
2025-02-07 23:17:48 +0800

    Implement breakpoint for WeatherHeader

--- src/components/WeatherHeader/WeatherHeader.tsx
+++ src/components/WeatherHeader/WeatherHeader.tsx
@@ -1,7 +1,8 @@
+import Grid from "@mui/material/Grid2";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import { WeatherResponse } from "../../types";
 import dayjs from "dayjs";
+import { WeatherResponse } from "../../types";
 
 interface WeatherHeaderProps extends WeatherResponse {
   datetime: number;
@@ -15,20 +16,36 @@ export const WeatherHeader = ({
   datetime,
 }: WeatherHeaderProps) => {
   return (
-    <Stack>
-      <Typography>Today's Weather</Typography>
-      <Typography>{temp}&deg;</Typography>
-      <Typography>
-        H: {temp_max}&deg; L: {temp_min}&deg;
-      </Typography>
-      <Stack direction="row">
+    <Grid container justifyContent="space-between">
+      <Grid
+        size={{
+          md: 6,
+        }}
+      >
+        <Typography>Today's Weather</Typography>
+        <Typography>{temp}&deg;</Typography>
+        <Typography>
+          H: {temp_max}&deg; L: {temp_min}&deg;
+        </Typography>
         <Typography>
           {name}, {country}
         </Typography>
-        <Typography>{dayjs(datetime).format("DD-MM-YYYY hh:mmA")}</Typography>
-        <Typography>Humidity: {humidity}%</Typography>
-        <Typography>{weather?.[0].main}</Typography>
-      </Stack>
-    </Stack>
+      </Grid>
+      <Grid size={6}>
+        <Stack
+          direction={{
+            xs: "column-reverse",
+            md: "row",
+          }}
+          justifyContent="space-between"
+          alignItems="end"
+          height="100%"
+        >
+          <Typography>{dayjs(datetime).format("DD-MM-YYYY hh:mmA")}</Typography>
+          <Typography>Humidity: {humidity}%</Typography>
+          <Typography>{weather?.[0].main}</Typography>
+        </Stack>
+      </Grid>
+    </Grid>
   );
 };
